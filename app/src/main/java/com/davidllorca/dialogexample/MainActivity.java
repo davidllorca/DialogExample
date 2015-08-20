ecb61d55e0ea8280aebaf34609e8166131da4d03
dllorca-baron <dllorca.baron@gmail.com>
2015-08-20 20:13:24 +0200

    ProgressDialog detailed bar example added

--- app/src/main/java/com/davidllorca/dialogexample/MainActivity.java
+++ app/src/main/java/com/davidllorca/dialogexample/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,6 +15,8 @@ public class MainActivity extends Activity {
 
     String[] items = {"Google", "Apple", "Microsoft"};
     boolean[] itemsChecked = new boolean[items.length];
+
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +45,28 @@ public class MainActivity extends Activity {
         }).start();
     }
 
+    public void onClick3(View view) {
+        showDialog(1);
+        progressDialog.setProgress(0);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i <= 15; i++){
+                    try{
+                        Log.d("onClick3", "on thread");
+                        // Simulate doing somthig
+                        Thread.sleep(1000);
+                        progressDialog.incrementProgressBy((int)(100/15));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                progressDialog.dismiss();
+            }
+        }).start();
+    }
+
     @Override
     protected Dialog onCreateDialog(int id) {
         switch (id) {
@@ -66,6 +91,25 @@ public class MainActivity extends Activity {
                                 Toast.makeText(getBaseContext(), items[which] + (isChecked ? " checked!" : " unchecked!"), Toast.LENGTH_SHORT).show();
                             }
                         }).create();
+            case 1:
+                progressDialog = new ProgressDialog(this);
+                progressDialog.setIcon(R.mipmap.ic_launcher);
+                progressDialog.setTitle("Downloading file...");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getBaseContext(), "Ok clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getBaseContext(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                return progressDialog;
         }
         return null;
     }
